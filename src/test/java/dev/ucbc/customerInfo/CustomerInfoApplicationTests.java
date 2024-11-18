0dc0ffb22faac043a548d29eef10b120719fda97
me <me@Pascal-Koko>
2024-11-18 10:45:49 +0200

    first state of project

---
+++ src/test/java/dev/ucbc/customerInfo/CustomerInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ucbc.customerInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
